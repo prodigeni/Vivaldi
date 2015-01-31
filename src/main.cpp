a83d6580ce7e8a38a740607c336ee2cc73329516
Jem Orgun <jeorgun@gmail.com>
2015-01-31 07:51:28 -0700

    Run files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include "parser.h"
 #include "repl.h"
+#include "builtins.h"
 
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
 {
-  il::run_repl();
+  if (argc > 2) {
+    std::cerr << "Usage: " << argv[0] << " [file]\n";
+    return 1;
+  }
+  if (argc == 1) {
+    il::run_repl();
+    return 0;
+  }
+
+  std::ifstream file{argv[1]};
+  auto tokens = il::parser::tokenize(file);
+  auto exprs = il::parser::parse(tokens);
+  for (const auto& i : exprs)
+    i->eval(il::builtin::g_base_env);
+  return 0;
 }
