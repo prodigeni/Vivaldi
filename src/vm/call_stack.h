1be15bfe1fe55ca135c917db01544987c566068c
Jem Orgun <jeorgun@gmail.com>
2015-02-07 08:42:12 -0700

    Rewrite call_stack as struct

--- src/vm/call_stack.h
+++ src/vm/call_stack.h
@@ -1,8 +1,11 @@
 #ifndef IL_VM_CALL_STACK_H
 #define IL_VM_CALL_STACK_H
 
-#include "value.h"
+#include "instruction.h"
+
 #include "symbol.h"
+#include "utils.h"
+#include "value.h"
 
 #include <boost/optional/optional.hpp>
 
@@ -10,22 +13,27 @@
 
 namespace il {
 
-class vm_instruction;
+namespace vm {
 
 class call_stack {
 public:
-private:
-  boost::optional<call_stack&> m_parent;
-  boost::optional<call_stack&> m_enclosing;
-  std::unordered_map<symbol, value::base* const> m_local;
-  std::unordered_map<symbol, value::base* const> m_parameters;
-  boost::optional<value::base&> m_self;
+  call_stack(std::shared_ptr<call_stack> parent,
+             std::shared_ptr<call_stack> enclosing,
+             vector_ref<instruction>     instr_ptr);
 
-  std::vector<value::base* const> m_pushed_args;
-  boost::optional<value::base&> m_pushed_self;
+  boost::optional<std::shared_ptr<call_stack> const> parent;
+  boost::optional<std::shared_ptr<call_stack> const> enclosing;
+  std::unordered_map<symbol, value::base* const> local;
+  std::unordered_map<symbol, value::base* const> parameters;
+  boost::optional<value::base&> self;
 
-  boost::optional<value::base&>  m_retrieved_val;
+  std::vector<value::base* const> pushed_args;
+  boost::optional<value::base&> pushed_self;
+
+  vector_ref<instruction> instr_ptr;
 };
+
+}
 
 }
 
