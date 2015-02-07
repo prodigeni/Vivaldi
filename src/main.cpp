4eee9582f655d82b3687a5ff66e1d4048aff9854
Jem Orgun <jeorgun@gmail.com>
2015-02-07 13:26:20 -0700

    Remove outdated REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "builtins.h"
 #include "gc.h"
 #include "parser.h"
-#include "repl.h"
 #include "vm/call_stack.h"
 #include "vm/instruction.h"
 
@@ -10,13 +9,9 @@
 
 int main(int argc, char** argv)
 {
-  if (argc > 2) {
-    std::cerr << "Usage: " << argv[0] << " [file]\n";
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <file>\n";
     return 1;
-  }
-  if (argc == 1) {
-    il::run_repl();
-    return 0;
   }
 
   std::ifstream file{argv[1]};
