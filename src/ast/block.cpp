8face8a076fdc77ae42a202bd25e688a18b1f99a
Jem Orgun <jeorgun@gmail.com>
2015-02-10 15:16:57 -0700

    Return nil on empty block

--- src/ast/block.cpp
+++ src/ast/block.cpp
@@ -10,6 +10,20 @@ ast::block::block(std::vector<std::unique_ptr<expression>>&& subexpressions)
 
 std::vector<vm::command> ast::block::generate() const
 {
+  // Conceptually, *every* block statement consists of
+  //   eblk
+  //   push_nil
+  //   expr_1
+  //   expr_2
+  //   ...
+  //   expr_n
+  //   lblk
+  // But since the only time the push_nil is actually used is when there are no
+  // expressions, and since in that case the e/lblk don't change any semantics,
+  // there's no reason not to special-case it
+  if (!m_subexpressions.size())
+    return { {vm::instruction::push_nil} };
+
   std::vector<vm::command> vec{ {vm::instruction::eblk} };
 
   for (const auto& i : m_subexpressions) {
